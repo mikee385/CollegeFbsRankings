73c5b08f0d88e9e848ab73bafd8ae9937e93b141
mikee385 <mikee385@gmail.com>
2015-11-25 11:08:10 -0600

    Cleans up the using statements.

--- CollegeFbsRankings/Rankings/ConferenceStrength.cs
+++ CollegeFbsRankings/Rankings/ConferenceStrength.cs
@@ -1,12 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using CollegeFbsRankings.Conferences;
-using CollegeFbsRankings.Games;
 using CollegeFbsRankings.Teams;
 
 namespace CollegeFbsRankings.Rankings
